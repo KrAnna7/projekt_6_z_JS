9caf4da292a80bd15d51789c52cc25733b93efc6
KrAnna7 <anna.dzioch@gmail.com>
2017-04-10 11:49:11 +0200

    dodanie nowych plików

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// plik scripts.js
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Ola'
+
+if (allNames.indexOf(newName) === -1) {
+	var x = allNames.push(newName)
+	console.log(x)
+	console.log(allNames)
+} else {
+	console.log ('Takie imię już się pojawiło')
+}
